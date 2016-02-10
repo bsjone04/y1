b5c9cb4384db03db786a320f25a249f604e2e884
Brent Jones <brent_jones2006@yahoo.com>
2016-02-10 04:52:56 -0500

    attempt to change paths 2

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,12 +20,12 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: '../views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
       .when('/about', {
-        templateUrl: 'views/about.html',
+        templateUrl: '../views/about.html',
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
