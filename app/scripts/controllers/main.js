5c94bfa1b48aa33362dea49dd0008118a347df20
Brent Jones <brent_jones2006@yahoo.com>
2016-02-10 03:44:55 -0500

    Yeoman Build 1

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name y1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the y1App
+ */
+angular.module('y1App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
